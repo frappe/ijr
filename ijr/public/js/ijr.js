9db71702977f0241d1477e077d6ac8b9b41e644c
Faris Ansari <netchamp.faris@gmail.com>
2023-03-14 20:02:58 +0530

    fix: update /states page as per new design

--- ijr/public/js/ijr.js
+++ ijr/public/js/ijr.js
@@ -16,18 +16,24 @@ function link_with_query_params() {
     }
   });
 
-  $(document).on("sl-change", "sl-select", (e) => {
-    let $el = $(e.target);
-    let query = $el.attr("query");
-    if (!query) {
-      return;
-    }
-    let key = query;
-    let value = e.target.value;
-    if (key && value) {
-      set_query_params(key, value);
-    }
+  [
+    {event: 'sl-change', selector: 'sl-select'},
+    {event: 'change', selector: 'select'},
+  ].forEach(({event, selector}) => {
+      $(document).on(event, selector, (e) => {
+        let $el = $(e.target);
+        let query = $el.attr("query");
+        if (!query) {
+          return;
+        }
+        let key = query;
+        let value = e.target.value;
+        if (key && value) {
+          set_query_params(key, value);
+        }
+      });
   });
+
 }
 
 function set_query_params(key, value, clear_others = false) {
